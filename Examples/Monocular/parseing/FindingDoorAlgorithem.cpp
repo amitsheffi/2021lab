231969eb575e29d672a6ba5c53faa0be0562b5a3
amit sheffi <33692259+amitsheffi@users.noreply.github.com>
2021-09-13 18:16:04 +0300

    Changing the main algorithm of finding door
    We now have 2 algorithms. The 1st algorithm is the interval algorithm and this is the algorithm we use. The second algorithm is the slicing algorithm.
    As of right now, both algorithms are giving out the same results more or less and we are using intervals.

--- Examples/Monocular/parseing/FindingDoorAlgorithem.cpp
+++ Examples/Monocular/parseing/FindingDoorAlgorithem.cpp
@@ -1,37 +1,46 @@
 // FindingDoorAlgorithem.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include "point.h"
 #include <algorithm>
-
+#include <math.h>
 
 using namespace std;
 vector<Point> parsePoints(string filePath);
 vector<Point> getMaxPoints(vector<Point>& points, Point targetP, int num);
 Point cleanPoints(vector<Point>& maxPoints);
 int getNeighbors(vector<Point>& points, Point& p);
+Point findAvaregePoint(vector<Point>& points);
+vector<Point> getMaximumInterval(vector<Point>& points, Point targetP, int intervalSize);
+Point getDoorUsingSlices(vector<Point>& points, Point targetP, int degreeOfSlice);
+bool sortFunction(Point p1, Point p2);
+void sortOutOutliers(vector<Point>& points, Point targetP, int minNumberOfNeighbors, double neighborDistance);
+void savePointToCsv(vector<Point>& points);
+Point getMaxPoint(vector<Point>& points, Point& targetP);
 
 int main()
 {
-	int doorPointIndex = 0;
-	int number = 10;
-	double angle;
 	Point p(0, 0, 0);
 	vector<Point> points = parsePoints("data\\pointDataVered.csv");
-	vector<Point> maxPoints = getMaxPoints(points, p, number);
-	int maxNeighbors = getNeighbors(points, maxPoints[0]);
-	for (int i = 1; i < number; i++)
+	sortOutOutliers(points, p, 10, 0.1);
+	//vector<Point> interval = getMaximumInterval(points, p, 10);
+	//cout << getMaxPoint(points, p) << endl;
+}
+
+Point getMaxPoint(vector<Point>& points, Point& targetP)
+{
+	Point max(0, 0, 0);
+	for (Point p : points)
 	{
-		if (getNeighbors(points, maxPoints[i]) > maxNeighbors)
-			doorPointIndex = i;
+		if (p.getDistanceFrom(targetP) > max.getDistanceFrom(targetP))
+			max = p;
 	}
-	//Point t = cleanPoints(maxPoints);
-	//angle = atan2(0, 0);
-	cout << maxPoints[doorPointIndex] << endl;
+	return max;
 }
 
 vector<Point> parsePoints(string filePath)
@@ -52,49 +61,68 @@ vector<Point> parsePoints(string filePath)
 
 	points.clear();
 
-	while (getline(fin, line)) // main loop
-	{
-		pos = 0;
-		counter = 0;
-		Point point(0, 0, 0);
-		while ((pos = line.find(delimiter)) != string::npos) {
-			token = line.substr(0, pos);
-			value = stod(token);
-			if (counter == 0)
-			{
-				point.setX(value);
-			}
-			else if (counter == 1)
-			{
-				point.setY(value);
-			}
-			line.erase(0, pos + delimiter.length());
-			counter++;
+while (getline(fin, line)) // main loop
+{
+	pos = 0;
+	counter = 0;
+	Point point(0, 0, 0);
+	while ((pos = line.find(delimiter)) != string::npos) {
+		token = line.substr(0, pos);
+		value = stod(token);
+		if (counter == 0)
+		{
+			point.setX(value);
 		}
-		value = stod(line);
-		point.setZ(value);
-		points.push_back(point);
+		else if (counter == 1)
+		{
+			point.setY(value);
+		}
+		line.erase(0, pos + delimiter.length());
+		counter++;
 	}
-	return points;
+	value = stod(line);
+	point.setZ(value);
+	points.push_back(point);
+}
+return points;
+}
+
+void savePointToCsv(vector<Point>& points)
+{
+	ofstream file1, file2;
+	Point z(0, 0, 0);
+	file1.open("C:\\Users\\ori.sheffi\\Desktop\\2021Lab\\FindingDoorAlgorithem\\csvData\\fileAllPoints.csv");
+	file2.open("C:\\Users\\ori.sheffi\\Desktop\\2021Lab\\FindingDoorAlgorithem\\csvData\\fileNoOutliers.csv");
+	for (Point& p : points)
+	{
+		file1 << p.getX() << ',' << p.getY() << ',' << p.getZ() << '\n';
+	}
+	sortOutOutliers(points, z, 10, 0.1);
+	for (Point& p : points)
+	{
+		file2 << p.getX() << ',' << p.getY() << ',' << p.getZ() << '\n';
+	}
 }
 
 vector<Point> getMaxPoints(vector<Point>& points, Point targetP, int num)
 {
 	vector<Point> maxPoints(num);
+	Point tmp1, tmp2;
 	for (int i = 0; i < points.size(); i++)
 	{
+		tmp1 = points[i];
 		for (int j = 0; j < num; j++)
 		{
-			if (points[i].getDistanceFrom(targetP) > maxPoints[j].getDistanceFrom(targetP))
+			if (tmp1.getDistanceFrom(targetP) > maxPoints[j].getDistanceFrom(targetP))
 			{
-				maxPoints[j] = points[i];
-				break;
+				tmp2 = maxPoints[j];
+				maxPoints[j] = tmp1;
+				tmp1 = tmp2;
 			}
 		}
 	}
 	return maxPoints;
 }
-
 
 Point cleanPoints(vector<Point>& maxPoints)
 {
@@ -118,4 +146,128 @@ int getNeighbors(vector<Point>& points, Point& p)
 		}
 	}
 	return counter - 1;
+}
+
+Point findAvaregePoint(vector<Point>& points)
+{
+	Point p;
+	double x = 0, y = 0, z = 0;
+	for (int i = 0; i < points.size(); i++)
+	{
+		x += points[i].getX();
+		y += points[i].getY();
+		z += points[i].getZ();
+	}
+	x = x / points.size();
+	y = y / points.size();
+	z = z / points.size();
+	p.setX(x);
+	p.setY(y);
+	p.setZ(z);
+	return p;
+}
+
+vector<Point> getMaximumInterval(vector<Point>& points, Point targetP, int intervalSize)
+{
+	int startingIntervalIndex = 0;
+	double sumMaxInterval = 0, tmpInterval;
+	vector<Point> interval;
+	for (int i = 0; i < points.size() - intervalSize + 1; i++)
+	{
+		tmpInterval = 0;
+		for (int j = i; (j - i) < intervalSize; j++)
+		{
+				tmpInterval += points[j].getDistanceFrom(targetP);
+		}
+		if (tmpInterval > sumMaxInterval)
+		{
+			sumMaxInterval = tmpInterval;
+			startingIntervalIndex = i;
+		}
+	}
+	for(int i = startingIntervalIndex; i - startingIntervalIndex < intervalSize; i++)
+	{
+			interval.push_back(points[i]);
+	}
+	return interval;
+}
+
+Point getDoorUsingSlices(vector<Point>& points, Point targetP, int degreeOfSlice)
+{
+	vector<vector<Point>> slices(360 / degreeOfSlice);
+	vector<Point> door;
+	double angle, maxSliceDistance = 0, sliceDist;
+	int degree;
+	int maxSliceIndex = 0, i = 0;
+	for (Point& p : points)
+	{
+		angle = atan2(p.getX() - targetP.getX(), p.getZ() - targetP.getZ());
+		angle = angle * 180 / M_PI;
+		angle += 180;
+		degree = floor(angle / (360 / degreeOfSlice));
+		slices[degree].push_back(p);
+	}
+	for (auto& slice : slices)
+	{
+		sliceDist = 0;
+		for (Point& p : slice)
+		{
+			sliceDist += p.getDistanceFrom(targetP);
+		}
+		sliceDist = sliceDist / slice.size();
+		if (sliceDist > maxSliceDistance)
+		{
+			maxSliceIndex = i;
+			maxSliceDistance = sliceDist;
+		}
+		i++;
+	}
+	door = getMaxPoints(slices[maxSliceIndex], targetP, 1);
+	return door[0];
+}
+
+void sortOutOutliers(vector<Point>& points, Point targetP, int minNumberOfNeighbors, double neighborDistance)
+{
+	int numOfNeighbors;
+	double currentDistance;
+	Point p(0, 0, 0);
+	vector<Point> goodPoints;
+	sort(points.begin(), points.end(), sortFunction);
+	for (int i = 0; i < points.size(); i++)
+	{
+		numOfNeighbors = 0;
+		currentDistance = points[i].getDistanceFrom(p);
+		for (int j = i + 1; j < points.size(); j++)
+		{
+			if (std::abs(currentDistance - points[j].getDistanceFrom(p)) > neighborDistance)
+				break;
+			if (points[i].getDistanceFrom(points[j]) < neighborDistance)
+				numOfNeighbors++;
+		}
+		for (int j = i - 1; j >= 0; j--)
+		{
+			if (std::abs(currentDistance - points[j].getDistanceFrom(p)) > neighborDistance)
+				break;
+			if (points[i].getDistanceFrom(points[j]) < neighborDistance)
+				numOfNeighbors++;
+		}
+		
+		if (numOfNeighbors >= minNumberOfNeighbors)
+			goodPoints.push_back(points[i]);
+		/*
+		if (numOfNeighbors < minNumberOfNeighbors)
+		{
+			points.erase(points.begin() + i);
+			i--;
+		}
+		*/
+	}
+	points = goodPoints;
+	//cout << goodPoints.size() << endl;
+}
+
+bool sortFunction(Point p1, Point p2)
+{
+	Point z(0, 0, 0);
+	return (p1.getDistanceFrom(z) < p2.getDistanceFrom(z));
 }
